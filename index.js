82c8e32cb65e2b89b754cd71794fc338524174c0
VZNRY1 <tajbland1@gmail.com>
2022-12-07 10:10:04 -0500

    created some psuedo code as well as typed out question into the questions array

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { features } = require('process');
 
+inquirer
+    .prompt([
+        {
+            type:
+            name:
+            message:
+        }
+
+    ])
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    //description
+    "Describe your project(why did you build this project and what was the motivation)?",
+    //Table of Contents
+
+    //Installation
+    "what are the steps required to install your projects? Please provide a step by step!",
+    //Usage
+"Please provide instructions and examples for use of your application",
+    //Credits
+"List your collaborators with links to their Github profiles.",
+    //License
+    //Badges
+    //Features
+"List the freatures of your project, if any",
+    //How to Contribute
+    "If you would like Contributions tell us how here, adn the guidelines for doing so",
+    //tests
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(README.md, JSON.stringify(data, null, '\t'), (err) =>
+err ? console.log(err) : console.log('Success!')
+);
 
 // TODO: Create a function to initialize app
 function init() {}
