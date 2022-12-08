1ca0d7d9fecf3086bbf1dc3e6bb2aaa29c448e98
VZNRY1 <tajbland1@gmail.com>
2022-12-08 00:47:22 -0500

    Added the questions to the inqurier fucntion as well as required the generate markdown js

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { features } = require('process');
+const { generatemarkdown } = require('./generateMarkdown');
 
 inquirer
     .prompt([
         {
-            type:
-            name:
-            message:
+            type:'input',
+            name:'Document Title',
+            message:'"What is the tile of your Project?',
+        },
+        {
+            type:'input',
+            name:'Project Description',
+            mesage:"Describe your project(why did you build this project and what was the motivation)?",
+        },
+        {
+            type:'input' ,
+            name:'Installation',
+            message:'What are the steps required to install your project? Please provide a step by step!',
+        },
+        {
+            type:'input',
+            name:'Usage',
+            message:'Please provide instructions and examples for use of your application!',
+        },
+        {
+            type:'input',
+            name:'Credit',
+            message:'List your collaborators with links to their Github profiles.',
+        },
+        {
+            type:'input',
+            name:'Features',
+            message:'List the features of your project',
+        },
+        {
+            type:'input',
+            name:'How to Contribute',
+            message:'If you would like Contributions tell us how here, adn the guidelines for doing so',
+        },
+        {
+            type:'input',
+            name:'Application Test',
+            message:'How can the user test this application?'
         }
 
     ])
@@ -34,7 +69,7 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(README.md, JSON.stringify(data, null, '\t'), (err) =>
+.then() =>fs.writeToFile(README.md, generatemarkdown(answers, null, '\t'), (err) =>
 err ? console.log(err) : console.log('Success!')
 );
 
